76f25c62b8b6e525054fba0953c94776c2f429f4
Malanius Privierre <malaniusprivierre@gmail.com>
2021-09-29 17:08:21 +0100

    Check if character is letter in alphabet

--- 01-poor-mans-bar-chart/src/poor_mans_bar_chart.py
+++ 01-poor-mans-bar-chart/src/poor_mans_bar_chart.py
@@ -1,14 +1,18 @@
 import pprint
 
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
 def count_occurences(sentence):
     graph = {}
     for char in sentence:
+        if char not in ALPHABET:
+            continue
         if char in graph.keys():
             graph[char] = graph[char] + [char]
         else:
             graph[char] = [char]
     return graph
 
-sentence = input("Intput sentence to analyze: ")
+sentence = input("Intput sentence to analyze: ").lower()
 printer = pprint.PrettyPrinter(indent=4)
 printer.pprint(count_occurences(sentence))
