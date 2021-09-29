92ac4f3995ef914fb81b9b46e0ad315ccdff30d6
Malanius Privierre <malaniusprivierre@gmail.com>
2021-09-29 16:35:16 +0100

    Resolve pylint and pydocstyle issues

--- 01-pig-latin/src/pig_latin.py
+++ 01-pig-latin/src/pig_latin.py
@@ -1,6 +1,9 @@
+"""Convert any english word to pig latin."""
+
 consonants = ["b","c","d","f","g","j","k","l","m","n","p","q","s","t","v","x","z","h","r","w","y"]
 
 def pig_latinize(word):
+    """Convert given word to pig latin."""
     start_letter = word[:1]
     rest_word = word[1:]
     if start_letter in consonants:
@@ -8,6 +11,7 @@ def pig_latinize(word):
     return word + "way"
 
 def main():
+    """Promts for word to be converted to pig latin."""
     word = input("Pick a word to convert to pig latin: ").lower()
     pig_latin_word = pig_latinize(word)
     print(f"Your word in pig latin: {pig_latin_word}")
