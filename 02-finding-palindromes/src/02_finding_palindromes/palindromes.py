c6c16f58ae560ab6c00110d0de88629ea32fa16e
Malanius Privierre <malaniusprivierre@gmail.com>
2021-10-26 18:06:16 +0200

    Exclude single letters from palindromes

--- 02-finding-palindromes/src/02_finding_palindromes/palindromes.py
+++ 02-finding-palindromes/src/02_finding_palindromes/palindromes.py
@@ -4,7 +4,7 @@ word_list = load_dictionary.load('2of4brif.txt')
 palindrome_list = []
 
 for word in word_list:
-    if word == word[::-1]:
+    if len(word) > 1 and word == word[::-1]:
         palindrome_list.append(word)
 
 print(palindrome_list)
